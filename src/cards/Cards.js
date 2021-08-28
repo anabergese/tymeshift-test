372b0ca3d5bfe44403c289b39d7f4c69bf7b85d8
ana bergese <violetagele22@gmail.com>
2021-08-28 20:35:55 +0200

    Added style to cards component

--- src/cards/Cards.js
+++ src/cards/Cards.js
@@ -3,14 +3,14 @@ import React from 'react';
 const Cards = () => {
   return(
     <div className="cards">
-      <p>edit icon</p>
-      <div className="card-description">
-        <h4 className="company-name">Acme HQ</h4>
-        <div>
+      {/*<p>edit icon</p>*/}
+      <div className="card">
+        <h4 className="location-name">Acme HQ</h4>
+        <div clasName="location-description">
           <ul>
-            <li><i></i> 114 Users</li>
-            <li><i></i> 2:32pm (GMT +01:00)</li>
-            <li><i></i> 1 View</li>
+            <li><img src="Users.svg" alt="Girl in a jacket"/> 114 Users</li>
+            <li><img src="Timezone.svg" alt="Girl in a jacket"/> 2:32pm (GMT +01:00)</li>
+            <li><img src="Views.svg" alt="Girl in a jacket"/> 1 View</li>
           </ul>
         </div>
       </div>
