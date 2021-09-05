2943bded9c978d85f66efa983ff538b85457aa82
ana bergese <violetagele22@gmail.com>
2021-09-05 12:29:29 +0200

    Ordered the display of the modal and their background with css

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,19 +8,25 @@ const Modal = ( { showModal, setShowModal, location, views } ) => {
   return(
     <div>
       { showModal ? (
-        <div className="modal">
-          <img className="close" src="Close.svg" alt="Close icon" />
-          <h4 className="location-name">{location.name}</h4>
-          <div className="location-description">
-            <ul>
-              <li><img src="Users.svg" alt="Users icon"/>{location.userCount}</li>
-              <li><img src="Timezone.svg" alt="Timezone icon"/>{hour}:{minutes}{hour >= 12 ? 'pm' : 'am'} (GMT +01:00)</li>
-              <li><img src="Views.svg" alt="Views icon"/>{views} View</li>
-            </ul>
-          </div>
-          <div className="description">
-            <h3>Description</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit mattis blandit neque, vulputate proin non natoque mi pellentesque nulla nostra molestie varius, duis ultricies netus eget ad ullamcorper sed libero taciti.</p>
+        <div className="modal-background">
+          <div className="modal-wrapper">
+            <div className="modal-title">
+              <img className="close" src="Close.svg" alt="Close icon"/>
+              <h4 className="location-name">{location.name}</h4>
+            </div>
+            <div className="modal-description">
+              <div className="location">
+                <ul>
+                  <li><img src="Users.svg" alt="Users icon"/>{location.userCount}</li>
+                  <li><img src="Timezone.svg" alt="Timezone icon"/>{hour}:{minutes}{hour >= 12 ? 'pm' : 'am'} (GMT +01:00)</li>
+                  <li><img src="Views.svg" alt="Views icon"/>{views} View</li>
+                </ul>
+              </div>
+              <div className="description">
+                <h3>Description</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit mattis blandit neque, vulputate proin non natoque mi pellentesque nulla nostra molestie varius, duis ultricies netus eget ad ullamcorper sed libero taciti.</p>
+              </div>
+            </div>
             <button className="btn-done">Done</button>
           </div>
         </div>
